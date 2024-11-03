fa953fcedf0469f575c9aab9e361cba314366599
Darkhackit <earthur510@gmail.com>
2024-11-03 15:58:36 +0000

    added authentication

---
+++ db/migration/000002_add_users.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "users" (
+     "id" bigserial PRIMARY KEY ,
+     "email" varchar UNIQUE NOT NULL ,
+     "username" varchar UNIQUE NOT NULL ,
+     "active" boolean,
+     "name" varchar,
+     "password" varchar NOT NULL ,
+     "password_changed_at" timestamp DEFAULT (now()),
+     "created_at" timestamp DEFAULT (now())
+);
+CREATE INDEX ON "users" ("id");
+
+ALTER TABLE "account" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+-- CREATE UNIQUE INDEX ON account ("owner","currency");
+
+ALTER TABLE "account" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner",currency)
---
+++ db/migration/000003_add_sessions.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "sessions" (
+     "id" uuid PRIMARY KEY ,
+     "username" varchar UNIQUE NOT NULL ,
+     "refresh_token" varchar NOT NULL ,
+     "user_agent" varchar NOT NULL ,
+     "client_ip" varchar NOT NULL ,
+     "is_blocked" boolean NOT NULL DEFAULT false,
+     "created_at" timestamp DEFAULT (now()),
+    "expires_at" timestamptz NOT NULL DEFAULT (now())
+);
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
